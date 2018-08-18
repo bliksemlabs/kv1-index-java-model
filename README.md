66238aaf9a34b1cffabf49bf13b4ec8c5b1735e8
Stefan de Konink <stefan@konink.de>
2018-08-18 18:12:44 +0200

    Update for KV6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# kv4-java-model
+# kv6-java-model
 
-Downloads the BISON KV4 XSD files from https://github.com/BISONNL/KV4 and generates java model using jaxb.
+Downloads the BISON KV6 XSD files from https://github.com/BISONNL/KV6 and generates java model using jaxb.
 
 You need xmlstarlet installed.
 
--- bin/download-extract.sh
+++ bin/download-extract.sh
@@ -4,22 +4,21 @@
 
 PROFILE_PATH="./profile"
 
-BRANCH="8.1.0.2"
-ZIPFILE="$BRANCH.zip"
+ZIPFILE="${GIT_BRANCH}.zip"
 
 if [ -f $ZIPFILE ]; then
   rm $ZIPFILE
 fi
 
-wget -q "http://github.com/BISONNL/KV4/archive/$ZIPFILE"
+wget -q "${GITHUB_URL}/archive/$ZIPFILE"
 
 echo "Removing contents in $PROFILE_PATH"
 rm -rf $PROFILE_PATH/*
 
-unzip $ZIPFILE "KV4-${BRANCH}/xsd/*" -d $PROFILE_PATH
+unzip $ZIPFILE "${BISON_REPO}-${GIT_BRANCH}/xsd/*" -d $PROFILE_PATH
 
 rm -r $DESTINATION_PATH
 mkdir -p $DESTINATION_PATH
-cp -r $PROFILE_PATH/KV4-${BRANCH}/xsd/* $DESTINATION_PATH/.
+cp -r $PROFILE_PATH/${BISON_REPO}-${GIT_BRANCH}/xsd/* $DESTINATION_PATH/.
 
 rm $ZIPFILE
