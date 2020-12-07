cee9f9333f8602554304cc47f5c8b033a60001d3
Stefan de Konink <stefan@konink.de>
2020-12-07 10:58:47 +0100

    Make it work again

--- bin/download-extract.sh
+++ bin/download-extract.sh
@@ -4,13 +4,15 @@
 
 PROFILE_PATH="./profile"
 
-ZIPFILE="${GIT_BRANCH}.zip"
+ZIPFILE="v${GIT_BRANCH}.zip"
 
 if [ -f $ZIPFILE ]; then
   rm $ZIPFILE
 fi
 
-wget -q "${GITHUB_URL}/archive/$ZIPFILE"
+URL="${GITHUB_URL}/archive/$ZIPFILE"
+
+wget -q "${URL}"
 
 echo "Removing contents in $PROFILE_PATH"
 rm -rf $PROFILE_PATH/*
@@ -20,5 +22,3 @@ unzip $ZIPFILE "${BISON_REPO}-${GIT_BRANCH}/xsd/*" -d $PROFILE_PATH
 rm -r $DESTINATION_PATH
 mkdir -p $DESTINATION_PATH
 cp -r $PROFILE_PATH/${BISON_REPO}-${GIT_BRANCH}/xsd/* $DESTINATION_PATH/.
-
-rm $ZIPFILE
